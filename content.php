c99af9bb43aff051d4eab9af01a7a0a174ecea43
joseccf10 <joseccf10@gmail.com>
2016-04-12 19:02:24 +0200

    Añadido y acabado footer.php,content.php sin acabar y header solocreado.

---
+++ content.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joseccf
+ * Date: 12/04/16
+ * Time: 18:46
+ */
---
+++ header.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joseccf
+ * Date: 11/04/16
+ * Time: 15:51
+ */
