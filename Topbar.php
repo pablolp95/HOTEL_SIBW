3dc6b12c6234f86706502f566a2d0f2e90bcc5f2
joseccf10 <joseccf10@gmail.com>
2016-04-23 16:33:48 +0200

    Topbar añadido

--- Topbar.php
+++ Topbar.php
@@ -3,11 +3,13 @@
 
 class TopBar
 {
+
     function printBar($page){
         echo '<section class="container-fluid topbar">
             <div class="row">
             ';
             $this->printVariableBar($page);
+            $this->printSlider();
             $this->printcontact();
         echo '</div>
             </section>';
@@ -30,13 +32,13 @@ class TopBar
     }
 
     function printVariableBar($page){
-        echo '<div class="col-sm-8">
+        echo '<div class="col-sm-4">
                          <ul class="topbar-links">';
         switch ($page){
             case 'rooms':
                 $this->allref();
                 break;
-            case 'rop-room':
+            case 'top-room':
                 $this->ref($page);
                 break;
             case 'double-room':
@@ -68,8 +70,61 @@ class TopBar
         }
         else if($page=='top-room'){
             echo '<li><a href=\'index.php?page=triple-room\'>Habitación triple</a></li>
-        <li><a href=\'index.php?page=double-room\'>Habitación doble</a></li>';
+            <li><a href=\'index.php?page=double-room\'>Habitación doble</a></li>';
         }
+    }
+    function printSlider()
+    {
+        echo "<div class='col-sm-4'><div id=\"myCarousel\" class=\"carousel slide\" data-ride=\"carousel\">
+        <!-- Indicators -->
+        <ol class=\"carousel-indicators\">
+            <li data-target=\"#myCarousel\" data-slide-to=\"0\" class=\"active\"></li>
+            <li data-target=\"#myCarousel\" data-slide-to=\"1\"></li>
+            <li data-target=\"#myCarousel\" data-slide-to=\"2\"></li>
+            <li data-target=\"#myCarousel\" data-slide-to=\"3\"></li>
+            <li data-target=\"#myCarousel\" data-slide-to=\"4\"></li>
+            <li data-target=\"#myCarousel\" data-slide-to=\"5\"></li>
+            <li data-target=\"#myCarousel\" data-slide-to=\"6\"></li>
+            <li data-target=\"#myCarousel\" data-slide-to=\"7\"></li>
+            <li data-target=\"#myCarousel\" data-slide-to=\"8\"></li>
+        </ol>
+        <!-- Wrapper for slides -->
+        <div class=\"carousel-inner\" role=\"listbox\">
+            <div class=\"item active\">
+                <img src=\"img/promotions/1.png\" alt=\"Image\">
+            </div>
+
+            <div class=\"item\">
+                <img src=\"img/promotions/2.png\" alt=\"Image\">
+            </div>
+
+            <div class=\"item\">
+                <img src=\"img/promotions/3.png\" alt=\"Image\">
+            </div>
+
+            <div class=\"item\">
+                <img src=\"img/promotions/4.png\" alt=\"Image\">
+            </div>
+
+            <div class=\"item\">
+                <img src=\"img/promotions/5.png\" alt=\"Image\">
+            </div>
+
+            <div class=\"item\">
+                <img src=\"img/promotions/6.png\" alt=\"Image\">
+            </div>
+            <div class=\"item\">
+                <img src=\"img/promotions/7.png\" alt=\"Image\">
+            </div>
+            <div class=\"item\">
+                <img src=\"img/promotions/8.png\" alt=\"Image\">
+            </div>
+
+        </div>
+
+        </div>
+       </div>
+";
     }
 }
 
